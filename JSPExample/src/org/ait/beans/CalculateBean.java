3fdff9144c3e482a1e964d7bb0a8bf2e38092e5a
Márk <Mrk@Marko>
2020-03-28 15:32:58 +0100

    f

---
+++ JSPExample/src/org/ait/beans/CalculateBean.java
@@ -0,0 +1,20 @@
+package org.ait.beans;
+
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+
+/**
+ * Session Bean implementation class CalculateBean
+ */
+@Stateless
+@LocalBean
+public class CalculateBean implements CalculateBeanLocal {
+
+    /**
+     * Default constructor. 
+     */
+    public CalculateBean() {
+        // TODO Auto-generated constructor stub
+    }
+
+}
---
+++ JSPExample/src/org/ait/beans/CalculateBeanLocal.java
@@ -0,0 +1,8 @@
+package org.ait.beans;
+
+import javax.ejb.Local;
+
+@Local
+public interface CalculateBeanLocal {
+
+}
